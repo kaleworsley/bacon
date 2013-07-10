34ff0b46fb9c232cd285b0eb1387a09283d307a9
Kale Worsley <kale.worsley@trineo.co.nz>
2013-07-11 00:51:36 +1200

    Mmmm....bacon.

---
+++ bacon.rb
@@ -0,0 +1,109 @@
+require 'pry'
+require 'set'
+
+class Actor
+  attr_accessor :name, :films
+
+  def initialize(name)
+    @name = name
+  end
+
+  def films
+    @films ||= Set.new
+  end
+
+  def add_film(film)
+    films << film
+    film.add_actor(self)
+    film
+  end
+
+  def distance_from(actor, depth = 0, found = {})
+    if self == actor
+      return depth
+    end
+
+    if !found.has_key? self
+      found[self] = depth
+
+      neighbours.each do |neighbour|
+        if neighbour != self
+          ret = neighbour.distance_from(actor, depth + 1, found)
+          return ret if !ret.nil?
+        end
+      end
+    end
+  end
+
+  def neighbours
+    @neighbours ||= Set.new(films.map {|f| f.actors.to_a }.flatten.uniq)
+  end
+end
+
+class Film
+  attr_accessor :title, :actors
+
+  def initialize(title)
+    @title = title
+  end
+
+  def actors
+    @actors ||= Set.new
+  end
+
+  def add_actor(actor)
+    actors << actor
+    actor
+  end
+end
+
+class ActorList
+  def actors
+    @actors ||= Set.new
+  end
+
+  def add_actor(actor)
+    self.actors << actor
+    actor
+  end
+end
+
+class FilmList
+  def films
+    @films ||= Set.new
+  end
+
+  def add_film(film)
+    self.films << film
+    film
+  end
+
+  def find(title)
+    films.find {|f| f.title == title } || add_film(Film.new(title))
+  end
+end
+
+actors = ActorList.new
+films = FilmList.new
+
+kevin = Actor.new("Kevin Bacon")
+actors.add_actor(kevin).tap do |actor|
+  actor.add_film(films.find('flatliners'))
+end
+
+actors.add_actor(Actor.new("Kiefer")).tap do |actor|
+  actor.add_film(films.find('cheesemongers'))
+  actor.add_film(films.find('flatliners'))
+end
+
+bruce = Actor.new("Bruce")
+actors.add_actor(bruce).tap do |actor|
+  actor.add_film(films.find('cheesemongers'))
+end
+
+#puts bruce.distance_from(kevin)
+
+binding.pry
+
+
+
